4b1a294fe95f5480bb9fdb38edd784fc270d4ef1
Chebrolu.krishna <ch.krishnavamsi216@gmail.com>
2025-02-26 17:43:34 +0530

    Introduction part

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './intro.css'
+import bg from '../../assets/image.png'
+import {Link} from 'react-scroll'
+import btnImg from '../../assets/hireme.png'
 const Intro = () => {
   return (
-    <div>
-      intro 
-    </div>
+    <sectio id="intro">
+      <div className="introContent">
+        <span className="hello">Hello</span>
+        <span className="introText">I'm <span className="introName">Vamsi</span> <br/> React Developer</span>
+        <p className="introPara">I am a skilled React Developer With Experience in creating <br/> visually appealing and user friendly websites</p>
+        <Link><button className="btn"><img src={btnImg} alt="Hire" className='btnImg' /> Hire Me</button></Link>
+      </div>
+      <img src={bg} alt="Profile" className='bg' />
+    </sectio>
   )
 }
 
