ce78013b9c09a7f4af7694cf111cb1bd818a5cd2
Chebrolu.krishna <ch.krishnavamsi216@gmail.com>
2025-02-27 16:08:53 +0530

    [add] footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './fotter.css'
+const footer = () => {
+  return (
+    <footer className='footer'>
+          Copyright &#169; 2023 Krishna Vamsi.All rights received
+    </footer>
+  )
+}
+
+export default footer
